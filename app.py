3ed36ce4318994f2cc0d445f4093ca94a536a529
Christopher Bridges <bridgchr@classwork.engr.oregonstate.edu>
2024-07-21 20:58:33 -0700

    create appy.py and .env files

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+PORT = os.getenv('PORT')
+
+# Configuration
+
+app = Flask(__name__)
+
+# Routes 
+
+@app.route('/')
+def root():
+    return "Welcome to the OSU CS 340 - Flask Tutorial!"
+
+# Listener
+
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', PORT))
+    #                                 ^^^^
+    #              You can replace this number with any valid port
+    
+    app.run(port=port, debug=True) 
