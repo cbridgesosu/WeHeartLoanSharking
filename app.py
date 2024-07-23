11801b136b223f2fe417aa8ee1e4c70930669972
Christopher Bridges <bridgchr@classwork.engr.oregonstate.edu>
2024-07-22 18:00:44 -0700

    created add_client page

--- app.py
+++ app.py
@@ -9,23 +9,42 @@ PORT = os.getenv('PORT')
 # temporary data until DB implemented
 enforcers = [
     {
-    "firstName": "Sergei",
-    "lastName": None,
-    "startDate": "1990-01-01"
+        "firstName": "Sergei",
+        "lastName": None,
+        "startDate": "1990-01-01"
     },
     {
-    "firstName": "Elena",
-    "lastName": "Stark",
-    "startDate": "2015-08-22"
+        "firstName": "Elena",
+        "lastName": "Stark",
+        "startDate": "2015-08-22"
     },
     {
-    "firstName": "Georg",
-    "lastName": "Rulin",
-    "startDate": "2007-02-27"
+        "firstName": "Georg",
+        "lastName": "Rulin",
+        "startDate": "2007-02-27"
     }
 ]
 
-
+clients = [
+    {
+        "firstName": "Jon",
+        "lastName": "Snow",
+        "inGoodStanding": True,
+        "loansRemaining": 100000
+    },
+    {
+        "firstName": "Cersei",
+        "lastName": "Lannister",
+        "inGoodStanding": False,
+        "loansRemaining": 150000
+    },
+    {
+        "firstName": "Ned",
+        "lastName": "Stark",
+        "inGoodStanding": False,
+        "loansRemaining": 55000
+    }
+]
 
 # Configuration
 
@@ -46,6 +65,16 @@ def add_enforcer():
                               "lastName": request.form.get("lastName"), 
                               "startDate": request.form.get("startDate")})
     return render_template("add_enforcer.j2", enforcers=enforcers)
+
+@app.route('/add_client', methods=["POST", "GET"])
+def add_client():
+    if request.method == "POST":
+            print("Client added.")
+            clients.append({"firstName": request.form.get("firstName"), 
+                              "lastName": request.form.get("lastName"), 
+                              "inGoodStanding": True,
+                              "loansRemaining": 0})
+    return render_template("add_client.j2", clients=clients)
 
 @app.route('/test')
 def test():
