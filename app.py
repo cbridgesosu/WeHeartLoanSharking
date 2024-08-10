c550a326ae0bb94f014ab844b1843ebc9148e916
Charles Holz <holzc@oregonstate.edu>
2024-08-10 12:29:24 -0700

    Renamed view and added a redirect at the end of the delete loan route.

--- app.py
+++ app.py
@@ -274,12 +274,13 @@ def add_loan():
     return render_template("add_loan.j2", loans=loans,  clients=clients)
 
 @app.route('/delete_loan/<int:loanID>')
-def delete_assignment(loanID):
+def delete_loan(loanID):
     # Query to delete loan entry with selected ID
     query_Delete_Loan = "DELETE FROM Loans WHERE loanID = '%s';"
     cur = mysql.connection.cursor()
     cur.execute(query_Delete_Loan, (loanID,))
     mysql.connection.commit()
+    return redirect('/add_loan')
 
 # Routes for Collections page
 @app.route('/add_collection', methods=["POST", "GET"])
